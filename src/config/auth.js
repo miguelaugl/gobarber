6c81dba3d7c32c16fcb139c0e408b35e30f2de6e
Miguel <miguelaugl@outlook.com>
2020-04-27 22:35:31 -0300

    feat: configuração inicial com upload de arquivos

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e1e61321b808ddfb0187958b8ca3479d',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
+
+import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+
+const { Router } = require('express');
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
